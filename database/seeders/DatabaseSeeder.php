be50467632ba324b0a8aa57b3663a1d118db58b9
wendwin <wendwin12@gmail.com>
2026-09-17 22:03:22 +0700

    feat: add role model and role relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([RoleSeeder::class]);
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role_id' => 1,
         ]);
     }
 }
